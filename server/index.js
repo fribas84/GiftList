7a60ffd6fce66701f5b8a2680f6a5fb79cd63af0
fribas84 <fribas84.gmail.com>
2023-04-20 17:47:36 -0300

    name verification in server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
+
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
+
 
 const port = 1225;
 
@@ -8,14 +14,19 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = merkleTree.getRoot();;
+
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const name = body.name;
+  console.log("name: ", name);
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  console.log("Proof: ",proof);
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  console.log("Verification: ", isInTheList );
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
