1999e85275b44ede0d59379c5d4f7f140953c133
fribas84 <fribas84.gmail.com>
2023-04-20 17:47:33 -0300

    added validation in server

--- utils/example.js
+++ utils/example.js
@@ -8,12 +8,15 @@ const merkleTree = new MerkleTree(niceList);
 // get the root
 const root = merkleTree.getRoot();
 
+console.log("Root: ",root);
 // find the proof that norman block is in the list 
 const name = 'Norman Block';
 const index = niceList.findIndex(n => n === name);
 const proof = merkleTree.getProof(index);
-
+console.log("Proof: ",proof);
 // verify proof against the Merkle Root
-console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+console.log("Verification: ",verifyProof(proof, name, root) ); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+// Faking 
+console.log("Verification faked: ",verifyProof(proof, "Norman Vlock", root) );
