5d5dd0a07fe04ae1fa1f6d40d9ba69c95f3a77ab
fribas84 <fribas84.gmail.com>
2023-04-27 15:14:05 -0300

    finish modal and call to server

--- client/merkle-tree-client/src/App.jsx
+++ client/merkle-tree-client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Search from './components/Search'
 import ResultModal from './components/ResultModal';
@@ -7,7 +7,25 @@ function App() {
   const [showModal,setShowModal] = useState(false);
   const [name,setName] = useState("");
   const [won,setWon] = useState(false);
-  const [checked,setChecked] = useState(false);
+  const [check,setCheck] = useState(false);
+  const [error, setError] = useState(false);
+  const [message,setMessage] = useState("");
+
+
+  useEffect(() => {
+    if(check){
+      
+      setShowModal(true);
+    }
+    else{
+      setShowModal(false);
+    }
+  }, [check])
+
+  useEffect(()=>{
+    setError(false);
+},[name])
+  
 
   return (
     <div className='container mx-auto'>
@@ -15,12 +33,20 @@ function App() {
       <Search 
         setName = {setName}
         name = {name}
+        setCheck = {setCheck}
+        error={error}
+        setError={setError}
+        setWon={setWon}
+        setMessage={setMessage}
       />
       <ResultModal
         name={name}
         won={won}
         showModal={showModal}
         setShowModal={setShowModal}
+        setCheck={setCheck}
+        setName={setName}
+        message={message}
       />
     </div>
   )
--- client/merkle-tree-client/src/components/ResultModal.jsx
+++ client/merkle-tree-client/src/components/ResultModal.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Modal from 'react-modal';
 
-const ResultModal = () => {
+
+
+
+
+const ResultModal = ({name,won,showModal,setShowModal,setCheck,setName,message}) => {
+  const handleClose = ()=>{
+    setName("");
+    setCheck(false);
+    setShowModal(false);
+  }
   return (
-    <div>Modal</div>
+    <Modal
+    isOpen={showModal}
+    onRequestClose={()=>setShowModal(false)}
+  
+    ariaHideApp={false}>
+      <div className='justify-center items-center flex flex-col h-3/4'>
+        <div className='self-center text-3xl font-semibold'>
+          {message}
+        </div>
+      
+      <button 
+        onClick={handleClose}
+        className="fluid px-4 text-xl text-white bg-red-600 border-l mt-10 rounded-lg ml-2">
+          Close
+      </button>
+      </div>
+
+    </Modal>
   )
 }
 
